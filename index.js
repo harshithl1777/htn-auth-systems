e234fd454510c3000a87c8b3e0af7785f055307f
Harshith Latchupatula <harshithl1777@gmail.com>
2021-09-08 14:48:28 -0400

    make formatting changes, add config folder

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const cors = require('cors');
 const path = require('path');
 require('dotenv').config({ path: './server/config/.env' });
 
-const cardsRouter = require('./server/routes/cards');
+const cardsRouter = require('./server/routers/cards');
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV !== 'production') app.use(cors());
+
 app.use('/api/cards', cardsRouter);
 
 app.get('/api/health', (req, res) => res.sendStatus(200));
 
 app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.info(`Listening on port ${port}`));
